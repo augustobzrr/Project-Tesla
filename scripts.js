d50ccbbf426489932fa9b53fe7d50e65974a3fd0
augustobzrr <augustobzrrsys@gmail.com>
2024-08-26 22:28:31 -0300

    Project-Tesla

---
+++ scripts.js
@@ -0,0 +1,14 @@
+//document = ir no html
+//querySelector = buscar o que quiser que ele busque
+
+
+let formulario = document.querySelector(".formulario")
+
+function aparecerFormulario() {
+    formulario.style.left = "35%"
+}
+
+function desaparecerFormulario() {
+    formulario.style.left = "-100%"
+}
+
